1a8255947b0da02e6fbe1d463a5daaca9018c15a
magnata19 <dehpacifico@gmail.com>
2024-12-11 22:38:24 -0300

    implementando grpc.

---
+++ src/main/java/com/github/magnata19/sec01/SimpleProtoDemo.java
@@ -0,0 +1,18 @@
+package com.github.magnata19.sec01;
+
+import com.davidson.models.PersonOuterClass;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SimpleProtoDemo {
+
+    private static final Logger log = LoggerFactory.getLogger(SimpleProtoDemo.class);
+
+    public static void main(String[] args) {
+        var person = PersonOuterClass.Person.newBuilder()
+                .setName("Davidson")
+                .setAge(25)
+                .build();
+        log.info("{}", person);
+    }
+}
